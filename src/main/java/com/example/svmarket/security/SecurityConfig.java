a893f08f198c7d76e3b08a3df98611ba0baef879
ThuyThanhN <22130283@st.hcmuaf.edu.vn>
2026-05-03 15:07:33 +0700

    update code

--- src/main/java/com/example/svmarket/security/SecurityConfig.java
+++ src/main/java/com/example/svmarket/security/SecurityConfig.java
@@ -104,9 +104,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://localhost:5174"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
